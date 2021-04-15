ceb8f049a44e23f9d37b04c0c2e72400a04c5ab5
Jordan Hawker <hawker.jordan@gmail.com>
2021-04-14 22:13:03 -0700

    2015 Jeff Runquist Cabernet Sauvignon

--- app/components/recipe-info.js
+++ app/components/recipe-info.js
@@ -1,13 +1,25 @@
 import Component from '@glimmer/component';
 
+function capitalize([firstLetter, ...restOfWord]) {
+  return firstLetter.toUpperCase() + restOfWord.join('');
+}
+
+function readableKey(key) {
+  let words = key.split('-');
+
+  return words.reduce(function (partialKey, word) {
+    if (partialKey.length) {
+      return partialKey.concat(' ', capitalize(word));
+    }
+    return capitalize(word);
+  }, '');
+}
+
 export default class IngredientInfoComponent extends Component {
   get ingredients() {
-    return Object.entries(this.args.recipe).map(function ([
-      [keyFirstLetter, ...restOfKey],
-      value,
-    ]) {
+    return Object.entries(this.args.recipe).map(function ([key, value]) {
       return {
-        type: keyFirstLetter.toUpperCase() + restOfKey.join(''),
+        type: readableKey(key),
         share: value,
       };
     });
